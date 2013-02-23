a582ebeff71c6b79b36d2a0e49169508690421d0
Yehonal <hw.2@hotmail.it>
2013-02-23 13:17:13 +0100

    implemented new way to versioning database
    you can dump/install/update(to implement) via bash scripts in sql folder that , in windows, are fully compatible with bash shell of required git-extensions

---
+++ sql/base/auth/rbac_security_level_groups.sql
@@ -0,0 +1,10 @@
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */; /*!40103 SET TIME_ZONE='+00:00' */; /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */; /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */; /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */; /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */; DROP TABLE IF EXISTS `rbac_security_level_groups`; /*!40101 SET @saved_cs_client = @@character_set_client */; /*!40101 SET character_set_client = utf8 */; CREATE TABLE `rbac_security_level_groups` ( `secId` int(10) unsigned NOT NULL COMMENT 'Security Level id', `groupId` int(10) unsigned NOT NULL COMMENT 'group id', PRIMARY KEY (`secId`,`groupId`), KEY `fk__rbac_security_level_groups__rbac_groups` (`groupId`), CONSTRAINT `fk__rbac_security_level_groups__rbac_groups` FOREIGN KEY (`groupId`) REFERENCES `rbac_groups` (`id`) ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Default groups to assign when an account is set gm level'; /*!40101 SET character_set_client = @saved_cs_client */; LOCK TABLES `rbac_security_level_groups` WRITE; /*!40000 ALTER TABLE `rbac_security_level_groups` DISABLE KEYS */; INSERT INTO `rbac_security_level_groups` VALUES (0,1),
+(1,1),
+(1,2),
+(2,1),
+(2,2),
+(2,3),
+(3,1),
+(3,2),
+(3,3),
+(3,4); /*!40000 ALTER TABLE `rbac_security_level_groups` ENABLE KEYS */; UNLOCK TABLES; /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */; /*!40101 SET SQL_MODE=@OLD_SQL_MODE */; /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */; /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */; /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
---
+++ sql/base/world/game_event_model_equip.sql
@@ -0,0 +1,10 @@
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */; /*!40103 SET TIME_ZONE='+00:00' */; /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */; /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */; /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */; /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */; DROP TABLE IF EXISTS `game_event_model_equip`; /*!40101 SET @saved_cs_client = @@character_set_client */; /*!40101 SET character_set_client = utf8 */; CREATE TABLE `game_event_model_equip` ( `eventEntry` tinyint(4) NOT NULL COMMENT 'Entry of the game event.', `guid` int(10) unsigned NOT NULL DEFAULT '0', `modelid` mediumint(8) unsigned NOT NULL DEFAULT '0', `equipment_id` mediumint(8) unsigned NOT NULL DEFAULT '0', PRIMARY KEY (`guid`) ) ENGINE=MyISAM DEFAULT CHARSET=utf8; /*!40101 SET character_set_client = @saved_cs_client */; LOCK TABLES `game_event_model_equip` WRITE; /*!40000 ALTER TABLE `game_event_model_equip` DISABLE KEYS */; INSERT INTO `game_event_model_equip` VALUES (25,12088,0,92),
+(25,12093,0,92),
+(25,12095,0,92),
+(25,79670,0,92),
+(25,79675,0,92),
+(25,79676,0,92),
+(25,79690,0,92),
+(25,79792,0,92),
+(25,79807,0,92),
+(25,79814,0,92); /*!40000 ALTER TABLE `game_event_model_equip` ENABLE KEYS */; UNLOCK TABLES; /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */; /*!40101 SET SQL_MODE=@OLD_SQL_MODE */; /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */; /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */; /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
